45e716528be7270ef1f04ec76f4a113f207f221e
Rupert Barrow <48629751+RupertBarrow@users.noreply.github.com>
2022-11-21 19:01:22 +0100

    Add command to regenerate the local cumulusci.yml

--- get_all_cumulusci_files.sh
+++ get_all_cumulusci_files.sh
@@ -3,14 +3,18 @@
 # Run this script to download all the files and overwrite any of them exisyting locally
 #
 
-BASE="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
+REPO="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
 BRANCH="main"
 FILES="config/cumulusci/cumulusci.template.yml"
 FILES="$FILES orgs/beta.json orgs/feature.json orgs/release.json orgs/dev.json"
 FILES="$FILES .github/workflows/beta.yml .github/workflows/feature.yml .github/workflows/release.yml .github/workflows/github-visualizer-diagram.yml"
 
+# DOwnload files individually
 for FILE in $FILES; do
-  URL="$BASE/$BRANCH/$FILE"
+  URL="$REPO/$BRANCH/$FILE"
   mkdir -p `dirname $FILE`
   wget $URL -O $FILE --quiet
 done
+
+# Regenerate the local cumulusci.yml
+cat ./cumulusci.base.yml ./config/cumulusci/cumulusci.template.yml > ./cumulusci.yml
