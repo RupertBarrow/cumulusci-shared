c6a4337df31ad30cb26a212cf2bf6f1379bde9cb
RupertBarrow <rupert.barrow@lilo.org>
2022-11-22 11:31:55 +0100

    refactor: Generate package name from input parameter

--- scripts/get_all_cumulusci_files.sh
+++ scripts/get_all_cumulusci_files.sh
@@ -3,7 +3,7 @@
 # Run this script to download all the files and overwrite any of them exisyting locally
 #
 
-PACKAGE_NAME=$1
+PACKAGE_NAME="$1"
 
 REPO="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
 BRANCH="main"
@@ -11,12 +11,12 @@ FILES="config/cumulusci.template.yml"
 FILES="$FILES orgs/beta.json orgs/feature.json orgs/release.json orgs/dev.json"
 FILES="$FILES .github/workflows/beta.yml .github/workflows/feature.yml .github/workflows/release.yml .github/workflows/github-visualizer-diagram.yml"
 
-# DOwnload files individually
+# Download files individually
 for FILE in $FILES; do
   URL="$REPO/$BRANCH/$FILE"
   mkdir -p `dirname $FILE`
   wget $URL -O $FILE --quiet
-  sed -e 's/%%PACKAGE_NAME%%/${PACKAGE_NAME}/g' -I '' $FILE
+  sed -e "s/%%PACKAGE_NAME%%/${PACKAGE_NAME}/g" -I '' $FILE
 done
 
 echo
