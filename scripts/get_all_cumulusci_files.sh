6dea313248359f052e6c17bc471e9bfd2140ba12
RupertBarrow <rupert.barrow@lilo.org>
2022-11-22 10:38:53 +0100

    fix: CumulusCI template YML moved

--- scripts/get_all_cumulusci_files.sh
+++ scripts/get_all_cumulusci_files.sh
@@ -5,7 +5,7 @@
 
 REPO="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
 BRANCH="main"
-FILES="config/cumulusci/cumulusci.template.yml"
+FILES="config/cumulusci.template.yml"
 FILES="$FILES orgs/beta.json orgs/feature.json orgs/release.json orgs/dev.json"
 FILES="$FILES .github/workflows/beta.yml .github/workflows/feature.yml .github/workflows/release.yml .github/workflows/github-visualizer-diagram.yml"
 
