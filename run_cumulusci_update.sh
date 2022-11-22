7d275adf219de212a9ea17328276a84cd9f6ac3a
RupertBarrow <rupert.barrow@lilo.org>
2022-11-22 11:22:30 +0100

    refactor: Generate package name from input parameter

--- run_cumulusci_update.sh
+++ run_cumulusci_update.sh
@@ -3,6 +3,8 @@
 # Run this script to download all the files and overwrite any of them exisyting locally
 #
 
+PACKAGE_NAME=$1
+
 REPO="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
 BRANCH="main"
 FILE="get_all_cumulusci_files.sh"
@@ -11,4 +13,4 @@ URL="$REPO/$BRANCH/$FILE"
 mkdir -p scripts
 wget $URL -O scripts/$FILE --quiet
 
-bash scripts/$FILE
+bash scripts/$FILE "$PACKAGE_NAME"
--- scripts/get_all_cumulusci_files.sh
+++ scripts/get_all_cumulusci_files.sh
@@ -3,6 +3,8 @@
 # Run this script to download all the files and overwrite any of them exisyting locally
 #
 
+PACKAGE_NAME=$1
+
 REPO="https://raw.githubusercontent.com/RupertBarrow/cumulusci-shared/"
 BRANCH="main"
 FILES="config/cumulusci.template.yml"
@@ -14,11 +16,9 @@ for FILE in $FILES; do
   URL="$REPO/$BRANCH/$FILE"
   mkdir -p `dirname $FILE`
   wget $URL -O $FILE --quiet
+  sed -e 's/%%PACKAGE_NAME%%/${PACKAGE_NAME}/g' -I '' $FILE
 done
 
-# Regenerate the local cumulusci.yml
-cat ./cumulusci.base.yml ./config/cumulusci.template.yml > ./cumulusci.yml
-
 echo
-echo "Files downloaded, cumulusci.yml regenerated"
+echo "CumulusCI configuration regenerated"
 echo
